2432556cbfc3e46b6b4646581bf8508f12698dde
Irena Shemesh <irena.shemesh@terem.com.au>
2019-12-16 12:20:30 +1100

    Update products and add stock

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :price, numericality: true
+  validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def price=(input)
+    input.delete!("$")
+    super
+  end
 end
